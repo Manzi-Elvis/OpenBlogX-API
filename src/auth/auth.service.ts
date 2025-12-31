3852a21b6299d66b6ebebdde587121079aa195d0
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-31 19:57:35 +0200

    auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import * as bcrypt from 'bcrypt'
+import { UsersService } from '../users/users.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(data: any) {
+    const hash = await bcrypt.hash(data.password, 10)
+
+    const user = await this.usersService.create({
+      email: data.email,
+      username: data.username,
+      passwordHash: hash,
+    })
+
+    return this.signToken(user.id)
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email)
+    if (!user) throw new UnauthorizedException()
+
+    const match = await bcrypt.compare(password, user.passwordHash)
+    if (!match) throw new UnauthorizedException()
+
+    return this.signToken(user.id)
+  }
+
+  private signToken(userId: string) {
+    return {
+      access_token: this.jwtService.sign({ sub: userId }),
+    }
+  }
+}

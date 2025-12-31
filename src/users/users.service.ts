e1e4604a507258db6da1b1aec7b2d33e35c6a757
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-31 19:52:55 +0200

    updated

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { User, UserDocument } from './users.schema'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  create(data: Partial<User>) {
+    return this.userModel.create(data)
+  }
+
+  findByEmail(email: string) {
+    return this.userModel.findOne({ email })
+  }
+
+  findById(id: string) {
+    return this.userModel.findById(id).select('-passwordHash')
+  }
+}

009cf08c37f34feccad17821fd5ff0c1ec264b21
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-02 13:24:12 +0200

    Posts Controller (routes)

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post as HttpPost,
+  Body,
+  UseGuards,
+  Req,
+  Param,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  // Admin / Author
+  @UseGuards(JwtAuthGuard)
+  @HttpPost()
+  create(@Body() dto: CreatePostDto, @Req() req) {
+    return this.postsService.create(dto, req.user.userId);
+  }
+
+  // Public
+  @Get()
+  findAll() {
+    return this.postsService.findAllPublished();
+  }
+
+  @Get(':slug')
+  findOne(@Param('slug') slug: string) {
+    return this.postsService.findBySlug(slug);
+  }
+}

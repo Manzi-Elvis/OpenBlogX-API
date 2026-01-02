59361651e729374ccdd7d1c7bf586af4af238ba1
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-02 22:58:29 +0200

    Upload controller

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { cloudinaryStorage } from './cloudinary.storage';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  @UseGuards(JwtAuthGuard)
+  @Post('image')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: cloudinaryStorage,
+      limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    }),
+  )
+  uploadImage(@UploadedFile() file: Express.Multer.File) {
+    return {
+      url: file.path,
+    };
+  }
+}

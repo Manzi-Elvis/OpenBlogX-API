97809ad9c2a619afdf2a16815d1d46033981a91a
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-02 23:19:30 +0200

    Added delete endpoint

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,13 +1,17 @@
 import {
   Controller,
   Post,
+  Body,
+  Delete,
   UseGuards,
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import cloudinary from '../config/cloudinary.config';
 import { cloudinaryStorage } from './cloudinary.storage';
+import { DeleteImageDto } from './dto/delete-image.dto';
 
 @Controller('upload')
 export class UploadController {
@@ -19,6 +23,14 @@ export class UploadController {
       limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
     }),
   )
+  @Delete('image')
+  async deleteImage(@Body() dto: DeleteImageDto) {
+    const result = await cloudinary.uploader.destroy(dto.public_id);
+    return {
+    success: result.result === 'ok',
+  };
+  }
+
   uploadImage(@UploadedFile() file: Express.Multer.File) {
     return {
       url: file.path,

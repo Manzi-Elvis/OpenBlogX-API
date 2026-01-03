72c8347429ca8e94f470601fc7d7bce9ea6b5d91
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-03 16:55:10 +0200

    Protecting Admin routes

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,7 +10,10 @@ import {
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
+
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}

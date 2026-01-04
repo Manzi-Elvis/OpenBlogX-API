f9cdd3c106231b53fe5b7526e27e62ab0c2de240
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-04 13:20:41 +0200

    ...

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
+
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles('admin')
 @Controller('media')
